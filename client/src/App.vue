e53f950ae347e3b0efc3265793547ae781955bc5
Thiago Vieira Chioda <thiagochioda@hotmail.com>
2022-05-14 19:09:10 -0300

    Commit vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,17 +1,20 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <img alt="Vue logo" src="./assets/logo.png">
+    <PostComponent />
+  </div>
+  
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PostComponent from './components/PostComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PostComponent
   }
-}
+};
 </script>
 
 <style>
--- client/src/components/PostComponent.vue
+++ client/src/components/PostComponent.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'PostComponent',
   props: {
     msg: String
   }
