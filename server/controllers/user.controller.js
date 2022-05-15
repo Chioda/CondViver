fcea9863ec6d3ae1246c8eb06efdb048764c007b
Thiago Vieira Chioda <thiagochioda@hotmail.com>
2022-05-14 21:30:21 -0300

    Commit vue2

---
+++ server/controllers/user.controller.js
@@ -0,0 +1,51 @@
+const User = require("../models/user.model");
+
+// ==> Usando conceito de Async e Await
+
+// ==> Método responsável por Criar um novo 'User':
+exports.registerNewUser = async (req, res) => {
+  try {
+    // => Antes vamos fazer uma verificação se o usuário já possui algum e-mail já cadastrado:
+    const isUser = await User.find({ email: req.body.email });
+    console.log(isUser);
+    if (isUser.length >= 1) {
+      return res
+        .status(409)
+        .json({ message: "Atenção! Este e-mail já possui registro!" });
+    }
+
+    const newUser = new User(req.body);
+    const user = await newUser.save();
+    const token = await newUser.generateAuthToken(); // ==> Aqui chamaremos o método que criamos no model
+    return res
+      .status(201)
+      .json({ message: "Usuário(a) criado(a) com sucesso!", user, token });
+  } catch (err) {
+    return res.status(400).json({ err });
+  }
+};
+
+// ==> Método responsável por realizar um novo login 'User':
+exports.loginUser = async (req, res) => {
+  try {
+    const { email } = req.body;
+    const { password } = req.body;
+    const user = await User.findByCredentials(email, password);
+    if (!user) {
+      return res.status(401).json({
+        error: "Erro ao Logar! Verifique as suas credenciais de autenticação!",
+      });
+    }
+    const token = await user.generateAuthToken();
+    return res
+      .status(201)
+      .json({ message: "Usuário(a) logado com sucesso!", user, token });
+  } catch (err) {
+    return res.status(400).json({ err });
+  }
+};
+
+// ==> Método responsável por retornar um determinado 'User'
+exports.returnUserProfile = async (req, res) => {
+  await res.json(req.userData);
+};
--- server/index.js
+++ server/index.js
@@ -21,6 +21,6 @@ if (process.env.NODE_ENV === 'production') {
   app.get(/.*/, (req, res) => res.sendFile(__dirname + '/public/index.html'));
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`));
